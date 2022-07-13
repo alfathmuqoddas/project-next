d936cf7d11da4e2d260c4fd6e99b1311ab462f61
alfathmuqoddas <alfath.muqoddas@gmail.com>
2022-07-13 16:21:27 +0700

    edit the navbar dev

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 		  <div className="container">
-		    <Link href="/"><a className="navbar-brand">Next Blog</a></Link>
+		    <Link href="/"><a className="navbar-brand">Next Blog Dev</a></Link>
 		    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 		      <span className="navbar-toggler-icon"></span>
 		    </button>
