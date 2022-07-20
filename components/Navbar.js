45bf5263b03b53773345f1a90482f5346b938383
alfathmuqoddas <alfath.muqoddas@gmail.com>
2022-07-20 13:58:54 +0700

    commiting adding image and image optimiziation

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,14 +6,9 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 		  <div className="container">
 		    <Link href="/"><a className="navbar-brand">Next Blog Dev</a></Link>
-		    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		      <span className="navbar-toggler-icon"></span>
-		    </button>
-		    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-		      <div className="navbar-nav">
-		        <Link href="/posts"><a className="nav-link">All Posts</a></Link>
-		      </div>
-		    </div>
+			<div className="navbar-nav">
+				<Link href="/posts"><a className="nav-link">All Posts</a></Link>
+			</div>
 		  </div>
 		</nav>
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Wrapper from '../components/Wrapper'
-import Link from 'next/link'
+import Head from 'next/head';
+import Wrapper from '../components/Wrapper';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -19,11 +19,22 @@ export default function Home({ users }) {
         <meta name="description" content="Checkout our cool page" key="desc" />
       </Head>
     <h1>List of Users</h1>
-    { users.map(user => (
-      <Link key={user.id} href={"/details/" + user.id}>
-          <a><h3>{user.name}</h3></a>
-      </Link>
-    ))}
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4">
+      { users.map(user => (
+        <div key={user.id} className="col gy-3">
+          <div className="card"  style={{ minHeight: "16rem" }}>
+          <img src={`https://picsum.photos/seed/${user.name}/640/360`} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <Link href={"/details/" + user.id}>
+                <a><h4 className='card-title'>{user.name}</h4></a>
+            </Link>
+          </div>
+        </div>
+        </div>
+      ))}
+      </div>
+    </div>
     </Wrapper>
   )
 }
